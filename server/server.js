206237ff4633f811e3bdcb9e96579778e43005c5
Nicholas Mendini <nicholas.mendini@gmail.com>
2020-12-09 17:25:05 -0600

    Cloned proxy

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const router = require('./router');
+
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(PUBLIC_DIR));
+
+// Handling asset requests for webpack bundles by passing off requests to the bundles router
+app.use('/bundles', router.bundles);
+// Handling AJAX requests to the API by passing off requests to the api router
+app.use('/api', router.api);
+
+app.get('/listing/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public/index.html'));
+});
+
+module.exports = app;
